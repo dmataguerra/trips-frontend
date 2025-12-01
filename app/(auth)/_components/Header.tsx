6c1c478d25340e3e1f9cf320ff958b029a57965a
DavidM <dmataguerra@outlook.com>
2025-11-30 20:51:34 -0600

    fix : remove unnecesary navbar items from header

--- app/(auth)/_components/Header.tsx
+++ app/(auth)/_components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
 import AuthButton from "./AuthButton";
 
@@ -18,26 +19,11 @@ export default function Header() {
   return (
     <Navbar shouldHideOnScroll className="bg-hero_bg ">
       <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-text_primary ml-2">S.A. de C.V.</p>
+        <Link href="/" className="flex items-center">
+          <AcmeLogo />
+          <p className="font-bold text-text_primary ml-2">S.A. de C.V.</p>
+        </Link>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-6" justify="center">
-        <NavbarItem>
-          <a className="text-text_primary/90 hover:text-text_primary" href="#footer">
-            Contáctanos
-          </a>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <a className="text-text_primary font-semibold" aria-current="page" href="#hero">
-            Boletos
-          </a>
-        </NavbarItem>
-        <NavbarItem>
-          <a className="text-text_primary/90 hover:text-text_primary" href="#destinations">
-            Destinos
-          </a>
-        </NavbarItem>
-      </NavbarContent>
       <NavbarContent justify="end" className="items-center">
         <NavbarItem className="hidden lg:flex">
           <a className="text-text_primary/90 hover:text-text_primary" href="/">Home</a>
