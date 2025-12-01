c318b35e7651f090c73756e05e0ffd6dfb878cb5
karenca01 <kcervantes01@alumnos.uaq.mx>
2025-11-30 21:15:16 -0600

    style: header of bookings page

--- app/dashboard/bookings/page.tsx
+++ app/dashboard/bookings/page.tsx
@@ -1,14 +1,9 @@
-import Header from "../_components/Header";
 import BookingsList from "./_components/BookingsList";
 
 export default function BookingsPage() {
     return (
         <div className="min-h-screen flex flex-col bg-gray-100">
-            <Header />
-
-            <div className="flex-1 p-6">
-                <BookingsList />
-            </div>
+            <BookingsList />
         </div>
     );
 }
